c6fd8c997b245a287d9b77d7926e740a571f9f45
gideon rotich <gideonrotich2001@gmail.com>
2024-04-09 23:40:23 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const elements = document.getElementById("main");
+const newHeader = document.createElement("h1");
+
+elements.remove();
+newHeader.textContent = "YOUR-NAME is the champion";
+newHeader.setAttribute('id', 'victory');
